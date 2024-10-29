b6b2b889d7dc2ff208933684a8e4397f3c2961c7
Carmen <carmeenrbu@gmail.com>
2024-10-29 18:59:09 +0100

    Lo he cambiado a.php
    he cambiado la extension a .php, con un  ejemplo a

--- Bloque_A/A_1.1.php
+++ Bloque_A/A_1.1.php
@@ -1,11 +1,21 @@
+<!--PHP--> 
+<?php
+    $num = 6;
+    $num1 = 2;
+    $resultado = $num + $num1;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <p>hola</p>
+<h2><?php echo $resultado; ?></h2>
 </body>
+
 </html>
