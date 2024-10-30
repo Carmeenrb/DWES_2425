545b9f18ccffbf964b940a83d92d0fb4cb739206
Carmen <carmeenrbu@gmail.com>
2024-10-30 12:24:07 +0100

    Realizar ejercicios
    Realizado los ejercicios 1.7 y 1.8

---
+++ Bloque_A/A_1.7.php
@@ -0,0 +1,24 @@
+<!--PHP-->
+<?php
+//Paso 1: Cambiamos el nombre a mi nombre
+    $name='Carmen';
+//Paso 2: Añadimos un caramelo favorito delante
+    $favorites =['Lemon','Chocolate','Toffe','Fudge',];
+?>
+
+<!--HTML-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actividad 7:Echo Shorthand</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+<h1>The Candy Store</h1>
+<h2>Welcome <?=$name?></h2>
+<!--La posicion 0 ahora es Lemon-->
+<p>Your favorite type of candy is: <?=$favorites[0]?></p>
+</body>
+</html>
