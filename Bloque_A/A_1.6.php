7b7e4ffa152d970dd6a3ef5fb043bfcd91cd1d31
Carmen <carmeenrbu@gmail.com>
2024-10-31 08:30:26 +0100

    carpeta css
    Nueva carpeta de css

--- Bloque_A/A_1.6.php
+++ Bloque_A/A_1.6.php
@@ -26,11 +26,11 @@
 <h1>The Candy Store</h1>
 <h2>Offers</h2>
     <p><?php echo $offers[0]['name']; ?> - $<?php echo $offers[0]['price']; ?></p>
-    <p><?php echo $offers[1]['name'];?> - $<?php echo $offers[1]['price']?></p>
-    <p><?php echo $offers[2]['name'];?> - $<?php echo $offers[2]['price']?></p>
+    <p><?php echo $offers[1]['name'];?> - $<?php echo $offers[1]['price'];?></p>
+    <p><?php echo $offers[2]['name'];?> - $<?php echo $offers[2]['price'];?></p>
 
     <!--Salida del nuevo elemento -->
-    <p><?php echo $offers[3]['name'];?> - $<?php echo $offers[3]['price']?></p>
+    <p><?php echo $offers[3]['name'];?> - $<?php echo $offers[3]['price'];?></p>
 
 
 
