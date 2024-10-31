855e104cf8ec2079d1fee6d89178aea1c518fac5
Carmen <carmeenrbu@gmail.com>
2024-10-31 10:55:05 +0100

    cambios 1.13
    Nuevos cambios

--- Bloque_A/A_1.13.php
+++ Bloque_A/A_1.13.php
@@ -1,11 +1,23 @@
+<!--PHP-->
+<?php
+
+?>
+
+<!--HTML-->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Libreria Carmela</title>
 </head>
 <body>
-    
+    <h1>Libreria Carmela</h1>
+    <h2>Ofertas por el dia del libro: </h2>
+    <ul>
+        <li>Novela Romantica:</li>
+        <li>Novela de Misterio:</li>
+        <li>Novela de Fantasia:</li>
+    </ul>
 </body>
 </html>
