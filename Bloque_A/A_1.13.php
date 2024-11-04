8126310caa9233f52ae26291210bb4577ac6851f
Carmen <carmeenrbu@gmail.com>
2024-11-04 13:11:58 +0100

    nuevos cambios
    nuevos cambios en el link

--- Bloque_A/A_1.13.php
+++ Bloque_A/A_1.13.php
@@ -1,6 +1,17 @@
 <!--PHP-->
 <?php
+//Saludos
 
+//Array de los tipos de novelas 
+$novelas=["Romantica","Misterio","Terror","Fantasía","Hitoricas","Aventuras"];
+$precios=[
+    'romantica'=> 20,
+    'misterio' => 15,
+    'terror' => 25,
+    'fantasia' => 30,
+    'historicas' => 18,
+    'aventuras' => 15,
+];
 ?>
 
 <!--HTML-->
@@ -15,9 +26,19 @@
     <h1>Libreria Carmela</h1>
     <h2>Ofertas por el dia del libro: </h2>
     <ul>
-        <li>Novela Romantica:</li>
-        <li>Novela de Misterio:</li>
-        <li>Novela de Fantasia:</li>
+        <li>Novela <?= $novelas[0]?>: <?= $precios['romantica']?> €</li>
+        <li>Novela de  <?= $novelas[1]?>: <?= $precios['misterio']?> €</li>
+        <li>Novela de  <?= $novelas[2]?>: <?= $precios['terror']?> €</li>
+        <li>Novela de  <?= $novelas[3]?>: <?= $precios['fantasia']?> €</li>
+        <li>Novela de  <?= $novelas[4]?>: <?= $precios['historicas']?> €</li>
+        <li>Novela de  <?= $novelas[5]?>: <?= $precios['aventuras']?> €</li>
     </ul>
+    <h2>Pack Ahorro 2 x 1</h2>
+    <ol>
+        <li>Pack </li>
+    <ol>
+
+    <h2>Descuento de un 20%</h2>
+
 </body>
 </html>
--- Bloque_A/A_1.2.php
+++ Bloque_A/A_1.2.php
@@ -15,7 +15,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Updating Variables</title>
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="RecursosA1/css/styles.css">
 </head>
 
 <body>
